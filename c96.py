4b339bce3a80eb8c3f7f68d878792ff508ee7921
satya <77478985+satyaRider@users.noreply.github.com>
2022-03-24 14:59:57 +0530

    Add files via upload

---
+++ c96.py
@@ -0,0 +1,9 @@
+pocketmoney=int(input("enter your pocket money: "))
+print(pocketmoney)
+
+if(pocketmoney>500):
+    print("you are a rich kid")
+elif(pocketmoney>100):
+    print("you have a good life")
+else:
+    print("i know how you feel")
